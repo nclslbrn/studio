953f0ae801e5e99afc6f127010dfa3be4c658eb0
nclslbrn <nclslbrn@gmail.com>
2017-07-19 16:31:05 +0200

    second commit

--- footer.php
+++ footer.php
@@ -17,12 +17,12 @@
 				<div class="site-info">
 					<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'studio' ) ); ?>"><?php
 						/* translators: %s: CMS name, i.e. WordPress. */
-						printf( esc_html__( 'Proudly powered by %s', 'studio' ), 'WordPress' );
+						printf( esc_html__( 'Fièrement propulsé par %s', 'studio' ), 'WordPress' );
 					?></a>
 					<span class="sep"> | </span>
 					<?php
 						/* translators: 1: Theme name, 2: Theme author. */
-						printf( esc_html__( 'Theme: %1$s by %2$s.', 'studio' ), 'studio', '<a href="https://automattic.com/">Underscores.me</a>' );
+						printf( esc_html__( 'Theme: %1$s by %2$s.', 'studio' ), 'studio', '<a href="https://github.com/nclslbrn/studio">Studio</a>' );
 					?>
 				</div><!-- .site-info -->
 			</footer><!-- #colophon -->

29c6d8b2510286feaf04cb876f09c558431f1ab7
nclslbrn <nclslbrn@gmail.com>
2017-07-19 16:38:58 +0200

    change footer theme link name

--- footer.php
+++ footer.php
@@ -22,7 +22,7 @@
 					<span class="sep"> | </span>
 					<?php
 						/* translators: 1: Theme name, 2: Theme author. */
-						printf( esc_html__( 'Theme: %1$s by %2$s.', 'studio' ), 'studio', '<a href="https://github.com/nclslbrn/studio">Studio</a>' );
+						printf( esc_html__( 'Theme: %1$s by %2$s.', 'studio' ), 'studio', '<a href="https://github.com/nclslbrn/studio">Nicolas Lebrun</a>' );
 					?>
 				</div><!-- .site-info -->
 			</footer><!-- #colophon -->
